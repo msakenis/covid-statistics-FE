f15851ec1251aa3861a6046292453eeb74a0bdab
MindaugasSak <m.sakenis@gmail.com>
2021-01-30 13:54:48 +0200

    clean up more

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,39 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
     extends: [
-        'plugin:react/recommended',
+        'airbnb-typescript',
+        'airbnb/hooks',
         'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/react',
         'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
     ],
+    plugins: ['react', '@typescript-eslint', 'jest'],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
         ecmaFeatures: {
             jsx: true,
         },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
     },
     rules: {
-        'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
+        'linebreak-style': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
     },
 };
