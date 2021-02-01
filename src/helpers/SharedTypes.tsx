888713442c9b00cab0450c5c14ac7f6a4d800c50
MindaugasSak <m.sakenis@gmail.com>
2021-02-01 16:43:38 +0200

    added stats type

--- src/helpers/SharedTypes.tsx
+++ src/helpers/SharedTypes.tsx
@@ -2,4 +2,13 @@ export interface CovidDataType {
     deaths: number[];
     cases: number[];
     categories: string[];
+    stats: StatsObj[];
 }
+
+export type StatsObj = {
+    id: number;
+    title: string;
+    icon: string;
+    stats: number;
+    change: number;
+};
