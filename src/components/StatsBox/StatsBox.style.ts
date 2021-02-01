a6cf97c7f0921860238530b2bf59086323e0140b
MindaugasSak <m.sakenis@gmail.com>
2021-02-01 16:46:02 +0200

    styled and dynamic color if increased or decreased

--- src/components/StatsBox/StatsBox.style.ts
+++ src/components/StatsBox/StatsBox.style.ts
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 export const FlexBox = styled.div`
     display: flex;
     width: 100%;
-    margin: 1.5em 0;
+    margin-bottom: 1.5em;
     flex-wrap: wrap;
+    @media (max-width: ${({ theme }) => theme.tablet}) {
+        justify-content: center;
+    }
 `;
 
 export const StatsBox = styled.div`
@@ -18,9 +21,21 @@ export const StatsBox = styled.div`
     flex-direction: column;
     justify-content: space-between;
     box-sizing: border-box;
+    min-width: 15em;
+    margin-top: 1.5em;
 
     &&:not(:last-child) {
         margin-right: 1.5em;
+        @media (max-width: ${({ theme }) => theme.mobile}) {
+            margin-right: 0;
+        }
+    }
+    @media (max-width: ${({ theme }) => theme.tablet}) {
+        margin-right: 1.5em;
+    }
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+        width: 100%;
+        margin-right: 0;
     }
 `;
 
@@ -51,18 +66,15 @@ export const StatsTitle = styled.p`
     font-weight: 600;
 `;
 
-export const StatsChangeDiv = styled.div`
+export const StatsChangeDiv = styled.div<{ increase: boolean }>`
     margin: 0 auto;
     min-width: 6.5em;
-    color: #689f13;
+    color: ${({ increase }) => (increase ? '#689f13' : '#ed5d4f')};
     font-weight: 700;
-`;
-
-export const ArrowSpan = styled.span`
-    font-size: 0.8rem;
-    padding-right: 0.4em;
-
-    &&:after {
-        content: '\u2BC5';
+    && span {
+        font-size: 0.8rem;
+        padding-right: 0.4em;
     }
 `;
+
+export const ArrowSpan = styled.span<{ increase: boolean }>``;
--- src/components/StatsBox/StatsBox.tsx
+++ src/components/StatsBox/StatsBox.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import * as S from './StatsBox.style';
+import { StatsObj } from '../../helpers/SharedTypes';
 
-type StatsObj = {
-    id: number;
-    title: string;
-    icon: string;
-    stats: string;
-    change: number;
-};
-
-interface StatsDataProps {
+interface StatsBoxProps {
     statsData: StatsObj[];
 }
 
-const StatsBox: React.FC<StatsDataProps> = ({ statsData }) => {
+const formatPerc = (number: number): string => {
+    return Math.abs(number).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 });
+};
+const StatsBox: React.FC<StatsBoxProps> = ({ statsData }) => {
     return (
         <S.FlexBox>
             {statsData &&
@@ -22,11 +18,11 @@ const StatsBox: React.FC<StatsDataProps> = ({ statsData }) => {
                         <S.ImgDiv>
                             <S.Image src={item.icon} alt={item.title} />
                         </S.ImgDiv>
-                        <S.StatsInfo>{item.stats}</S.StatsInfo>
+                        <S.StatsInfo>{Math.ceil(item.stats)}</S.StatsInfo>
                         <S.StatsTitle>{item.title}</S.StatsTitle>
-                        <S.StatsChangeDiv>
-                            <S.ArrowSpan></S.ArrowSpan>
-                            {item.change}
+                        <S.StatsChangeDiv increase={item.change <= 0}>
+                            {item.change && <span>{item.change > 0 ? '\u2BC5' : '\u2BC6'}</span>}
+                            {formatPerc(item.change)}
                         </S.StatsChangeDiv>
                     </S.StatsBox>
                 ))}
